4b921d4fa612d3f3d1bb08c40fb72230eccda11b
bingyu <bingyu.zhou@siemens.com>
2018-10-30 16:59:53 +0100

    kd tree

--- util/kdtree.h
+++ util/kdtree.h
@@ -3,8 +3,26 @@
  */
 #include <vector>
 
-class kdtree
+template <class PointType>
+class Node
 {
-    void build_tree(const std::vector<double> &node_list, const int &depth);
-    void
+  Node *m_left, *m_right;
+  PointType m_value;
+
+public:
+  Node(const PointType &value, const Node *left = NULL, const Node *right = NULL);
+  ~Node();
+};
+
+template <class PointType>
+class KDtree
+{
+  Node *m_root;
+
+public:
+  KDtree();
+  ~KDtree();
+  KDtree(const std::vector<PointType> &point_list);
+  void build_tree(typename std::vector<PointType>::iterator &point_start, const int length, const int &depth);
+  PointType nearest_neighbor(const PointType &node_new);
 };
