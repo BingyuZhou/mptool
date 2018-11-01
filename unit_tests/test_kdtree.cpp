f3ad395dcbc4c249b0932f324e00f377afe5a47d
bingyu <zbyjeff@gmail.com>
2018-11-01 22:15:39 +0100

    build kd tree works!

--- unit_tests/test_kdtree.cpp
+++ unit_tests/test_kdtree.cpp
@@ -1,2 +1,26 @@
-#include <GUnit/GTest.h>
 #include <GUnit/GTest-Lite.h>
+#include <GUnit/GTest.h>
+#include <tuple>
+#include "../util/kdtree.h"
+
+typedef std::array<int, 2> point_2d;
+
+GTEST("test_kd_tree") {
+  KDtree<point_2d> my_kdtree;
+
+  SHOULD("return_correct_tree_structure") {
+    std::vector<point_2d> point_list{{2, 3}, {5, 4}, {9, 6},
+                                     {4, 7}, {8, 1}, {7, 2}};
+    int depth = 0;
+    Node<point_2d>* root =
+        my_kdtree.build_tree(point_list.begin(), point_list.size(), depth);
+    EXPECT_EQ(root->m_value[0], 7);
+    EXPECT_EQ(root->m_value[1], 2);
+    EXPECT_EQ(root->m_left->m_value[0], 5);
+    EXPECT_EQ(root->m_left->m_value[1], 4);
+    EXPECT_EQ(root->m_right->m_value[0], 9);
+    EXPECT_EQ(root->m_right->m_value[1], 6);
+    EXPECT_EQ(root->m_right->m_left->m_value[0], 8);
+    EXPECT_EQ(root->m_left->m_left->m_value[0], 2);
+  }
+}
--- util/kdtree.h
+++ util/kdtree.h
@@ -1,28 +1,55 @@
 /**
  * Balanced K-D tree
  */
+#include <algorithm>
 #include <vector>
 
 template <class PointType>
-class Node
-{
+class Node {
+ public:
   Node *m_left, *m_right;
   PointType m_value;
-
-public:
-  Node(const PointType &value, const Node *left = NULL, const Node *right = NULL);
-  ~Node();
+  Node(){};
+  Node(const PointType &value, Node *left = NULL, Node *right = NULL)
+      : m_value(value), m_left(left), m_right(right){};
+  ~Node(){};
 };
 
 template <class PointType>
-class KDtree
-{
-  Node *m_root;
+class KDtree {
+  Node<PointType> *m_root;
 
-public:
-  KDtree();
-  ~KDtree();
-  KDtree(const std::vector<PointType> &point_list);
-  void build_tree(typename std::vector<PointType>::iterator &point_start, const int length, const int &depth);
-  PointType nearest_neighbor(const PointType &node_new);
+ public:
+  KDtree(){};
+  ~KDtree(){};
+  KDtree(const std::vector<PointType> &point_list) {
+    build_tree(point_list.begin(), point_list.size(), 0);
+  };
+
+  Node<PointType> *build_tree(
+      const typename std::vector<PointType>::iterator &point_start,
+      const int length, int &depth) {
+    if (length == 0) return NULL;
+
+    int dimention = (*point_start).size();
+
+    int axis = depth % dimention;
+
+    std::sort(point_start, point_start + length,
+              [&](const PointType &n1, const PointType &n2) {
+                return n1[axis] < n2[axis];
+              });  // O(NlogN)
+
+    int median = length / 2;
+
+    Node<PointType> *current_node =
+        new Node<PointType>(*(point_start + median));
+    current_node->m_left = build_tree(point_start, median, ++depth);
+    current_node->m_right =
+        build_tree(point_start + median + 1, length - median - 1, ++depth);
+
+    return current_node;
+  };
+
+  PointType nearest_neighbor(const PointType &node_new){};
 };
