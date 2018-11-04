a3b3668a9da1449689579b695cdfa3ef801330a2
bingyu <zbyjeff@gmail.com>
2018-11-04 22:24:47 +0100

    add neareast neighbor

--- util/kdtree.h
+++ util/kdtree.h
@@ -1,6 +1,7 @@
 /**
  * Balanced K-D tree
  */
+#include <assert.h>
 #include <algorithm>
 #include <vector>
 
@@ -8,6 +9,7 @@ template <class PointType>
 class Node {
  public:
   Node *m_left, *m_right;
+  int distance;
   PointType m_value;
   Node(){};
   Node(const PointType &value, Node *left = NULL, Node *right = NULL)
@@ -24,6 +26,10 @@ class KDtree {
   ~KDtree(){};
   KDtree(const std::vector<PointType> &point_list) {
     build_tree(point_list.begin(), point_list.size(), 0);
+  };
+
+  void construct_tree(const typename std::vector<PointType> &point_list) {
+    m_root = build_tree(point_list.begin(), point_list.size(), 0);
   };
 
   Node<PointType> *build_tree(
@@ -51,5 +57,37 @@ class KDtree {
     return current_node;
   };
 
-  PointType nearest_neighbor(const PointType &node_new){};
+  PointType nearest_neighbor(const PointType &node_new) {
+    // Find the nearest neighbor of the node_new in the kd tree
+
+    PointType nearest = m_root->m_value;
+    auto calc_distance =
+        [](const PointType &n1, const PointType &n2) {
+          assert(n1.size() == n2.size());
+          int dis = 0;
+
+          for (int i = n1.size(); i >= 0; --i) {
+            dis += pow(n1[i] - n2[i]);
+          }
+          return dis;
+        }
+
+    std::vector<Node *>
+        min_heap{m_root};
+    int shortest_distance = calc_distance(node_new, m_root->m_value);
+
+    while (!min_heap.empty()) {
+      std::pop_heap(min_heap.begin(), min_heap.end(), [](Node *n1, Node *n2) {
+        return n1->distance < n2.distance;
+      });
+      Node *current_node = min_heap.back();
+      min_heap.pop_back();
+
+      int distance = calc_distance(node_new, current_node->m_value);
+      if (distance < shortest_distance) {
+        shortest_distance = distance;
+        nearest = current_node->m_value;
+      }
+    }
+  };
 };
