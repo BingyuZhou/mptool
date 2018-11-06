e7ff0d5a87393b99aca0a91b774d5cfbdd6dfcf1
bingyu <bingyu.zhou@siemens.com>
2018-11-06 16:35:07 +0100

    rrt base

--- rrt/rrt.h
+++ rrt/rrt.h
@@ -1,5 +1,22 @@
 #include <vector>
+#include "../util/kdtree.h"
 
-class rrt
+template <class PointType>
+class rrt_node
 {
+public:
+  rrt_node *m_parent;
+  std::vector<rrt_node *> m_children;
+  rrt_node();
+  rrt_node(rrt_node *parent, std::vector<rrt_node *> children);
+  rrt_node(rrt_node *parent, rrt_node *child);
+};
+
+template <class PointType>
+class rrt : private KDtree
+{
+public:
+  PointType random_sample();
+  bool obstacle_free();
+  virtual void extend(rrt_node<PointType> *sampled_node);
 };
