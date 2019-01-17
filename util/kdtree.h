e3bef83193c5e702ed45779a16eca0b551fb3131
bingyu <bingyu.zhou@siemens.com>
2019-01-17 16:43:35 +0100

    add comment

--- util/kdtree.h
+++ util/kdtree.h
@@ -1,5 +1,6 @@
 /**
  * Balanced K-D tree
+ * https://en.wikipedia.org/wiki/K-d_tree
  */
 #pragma once
 
@@ -8,8 +9,9 @@
 #include "utils.h"
 
 template <class PointType>
-class Node {
- public:
+class Node
+{
+public:
   Node *m_left, *m_right, *m_parent;
   int m_distance;
   int m_split_axis;
@@ -22,13 +24,16 @@ class Node {
 };
 
 template <class PointType>
-class KDtree {
+class KDtree
+{
   Node<PointType> *m_root;
 
   Node<PointType> *build_tree(
       const typename std::vector<PointType>::iterator &point_start,
-      const int length, int depth) {
-    if (length == 0) return NULL;
+      const int length, int depth)
+  {
+    if (length == 0)
+      return NULL;
 
     int dimention = (*point_start).size();
 
@@ -37,7 +42,7 @@ class KDtree {
     std::sort(point_start, point_start + length,
               [&](const PointType &n1, const PointType &n2) {
                 return n1[axis] < n2[axis];
-              });  // O(NlogN)
+              }); // O(NlogN)
 
     int median = length / 2;
 
@@ -45,55 +50,69 @@ class KDtree {
         new Node<PointType>(*(point_start + median));
     current_node->m_split_axis = axis;
     current_node->m_left = build_tree(point_start, median, ++depth);
-    if (current_node->m_left) current_node->m_left->m_parent = current_node;
+    if (current_node->m_left)
+      current_node->m_left->m_parent = current_node;
     current_node->m_right =
         build_tree(point_start + median + 1, length - median - 1, ++depth);
-    if (current_node->m_right) current_node->m_right->m_parent = current_node;
+    if (current_node->m_right)
+      current_node->m_right->m_parent = current_node;
 
     return current_node;
   };
 
- public:
+public:
   KDtree(){};
   ~KDtree(){};
 
   Node<PointType> *get_root() { return m_root; };
 
-  void construct_tree(typename std::vector<PointType> &point_list) {
+  void construct_tree(typename std::vector<PointType> &point_list)
+  {
     m_root = build_tree(point_list.begin(), point_list.size(), 0);
   };
 
-  void add_node(PointType node_add) {
+  void add_node(PointType node_add)
+  {
     int dimention = node_add.size();
     if (m_root == NULL)
       m_root = new Node<PointType>(node_add);
-    else {
+    else
+    {
       Node<PointType> *node_tmp = m_root;
-      while (true) {
+      while (true)
+      {
         if (node_add[node_tmp->m_split_axis] <=
-            node_tmp->m_value[node_tmp->m_split_axis]) {
-          if (node_tmp->m_left == NULL) {
+            node_tmp->m_value[node_tmp->m_split_axis])
+        {
+          if (node_tmp->m_left == NULL)
+          {
             node_tmp->m_left = new Node<PointType>(node_add);
             node_tmp->m_left->m_split_axis =
                 (node_tmp->m_split_axis + 1) % dimention;
             break;
-          } else
+          }
+          else
             node_tmp = node_tmp->m_left;
-        } else {
-          if (node_tmp->m_right == NULL) {
+        }
+        else
+        {
+          if (node_tmp->m_right == NULL)
+          {
             node_tmp->m_right = new Node<PointType>(node_add);
             node_tmp->m_right->m_split_axis =
                 (node_tmp->m_split_axis + 1) % dimention;
             break;
-          } else
+          }
+          else
             node_tmp = node_tmp->m_right;
         }
       }
     }
   }
 
-  PointType nearest_neighbor(const PointType &node_new) {
-    // Find the nearest neighbor of the node_new in the kd tree
+  PointType nearest_neighbor(const PointType &node_new)
+  {
+    // Find the nearest neighbor of the node_new in the kd tree O(logN)
 
     PointType nearest = m_root->m_value;
 
@@ -101,7 +120,8 @@ class KDtree {
     int shortest_distance = euclidian_dis(node_new, m_root->m_value);
     m_root->m_distance = shortest_distance;
 
-    while (!min_heap.empty()) {
+    while (!min_heap.empty())
+    {
       std::pop_heap(min_heap.begin(), min_heap.end(),
                     [](Node<PointType> *n1, Node<PointType> *n2) {
                       return n1->m_distance > n2->m_distance;
@@ -109,7 +129,8 @@ class KDtree {
       Node<PointType> *current_node = min_heap.back();
       min_heap.pop_back();
 
-      if (current_node->m_distance < shortest_distance) {
+      if (current_node->m_distance < shortest_distance)
+      {
         shortest_distance = current_node->m_distance;
         nearest = current_node->m_value;
       }
@@ -120,14 +141,11 @@ class KDtree {
                   node_new[current_node->m_parent->m_split_axis];
         return dis * dis;
       };
+
+      // Check if node_new and current_node are at different side of current_node->parent
       auto is_parent_middle = [&]() {
-        return (node_new[current_node->m_parent->m_split_axis] -
-                current_node->m_parent
-                    ->m_value[current_node->m_parent->m_split_axis]) *
-                           (current_node->m_parent->m_value
-                                [current_node->m_parent->m_split_axis] -
-                            current_node->m_value[current_node->m_parent
-                                                      ->m_split_axis]) >
+        return (node_new[current_node->m_parent->m_split_axis] - current_node->m_parent->m_value[current_node->m_parent->m_split_axis]) *
+                           (current_node->m_parent->m_value[current_node->m_parent->m_split_axis] - current_node->m_value[current_node->m_parent->m_split_axis]) >
                        0
                    ? true
                    : false;
@@ -143,22 +161,29 @@ class KDtree {
       int dis_to_hyperplane =
           current_node->m_value[current_node->m_split_axis] -
           node_new[current_node->m_split_axis];
+
+      // If not intersected, only consider one branch
       if (shortest_distance <=
-          dis_to_hyperplane * dis_to_hyperplane)  // compare square distance
+          dis_to_hyperplane * dis_to_hyperplane) // compare square distance
       {
         if (node_new[current_node->m_split_axis] <
-            current_node->m_value[current_node->m_split_axis]) {
-          if (current_node->m_left) {
+            current_node->m_value[current_node->m_split_axis])
+        {
+          if (current_node->m_left)
+          {
             current_node->m_left->m_distance =
                 euclidian_dis(node_new, current_node->m_left->m_value);
             min_heap.push_back(current_node->m_left);
             std::push_heap(min_heap.begin(), min_heap.end(),
                            [](Node<PointType> *n1, Node<PointType> *n2) {
-                             return n1->m_distance < n2->m_distance;
+                             return n1->m_distance > n2->m_distance;
                            });
           }
-        } else {
-          if (current_node->m_right) {
+        }
+        else
+        {
+          if (current_node->m_right)
+          {
             current_node->m_right->m_distance =
                 euclidian_dis(node_new, current_node->m_right->m_value);
             min_heap.push_back(current_node->m_right);
@@ -168,8 +193,12 @@ class KDtree {
                            });
           }
         }
-      } else {
-        if (current_node->m_left) {
+      }
+      // If intersected, consider both branches
+      else
+      {
+        if (current_node->m_left)
+        {
           current_node->m_left->m_distance =
               euclidian_dis(node_new, current_node->m_left->m_value);
           min_heap.push_back(current_node->m_left);
@@ -178,7 +207,8 @@ class KDtree {
                            return n1->m_distance > n2->m_distance;
                          });
         }
-        if (current_node->m_right) {
+        if (current_node->m_right)
+        {
           current_node->m_right->m_distance =
               euclidian_dis(node_new, current_node->m_right->m_value);
           min_heap.push_back(current_node->m_right);
--- util/utils.h
+++ util/utils.h
@@ -6,18 +6,21 @@
 #include "obstacle_sym.h"
 
 template <class T>
-float euclidian_dis(const T &n1, const T &n2) {
+float euclidian_dis(const T &n1, const T &n2)
+{
   assert(n1.size() == n2.size());
   float dis = 0.0f;
 
-  for (int i = n1.size() - 1; i >= 0; --i) {
-    dis += (n1[i] - n2[i]) * (n1[i] - n2[i]);  // Euclidean distance
+  for (int i = n1.size() - 1; i >= 0; --i)
+  {
+    dis += (n1[i] - n2[i]) * (n1[i] - n2[i]); // Euclidean distance
   }
   return std::sqrt(dis);
 }
 
 template <class T>
-bool inside_obstacle_area(const obstacle *obs, T point) {
+bool inside_obstacle_area(const obstacle *obs, T point)
+{
   if (point[0] > obs->top_left.first && point[0] < obs->bottom_right.first &&
       point[1] > obs->bottom_right.second && point[1] < obs->top_left.second)
     return true;
