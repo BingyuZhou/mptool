5518ae6bc2d3e19bdd6487ad041c1de5e4ca3bec
bingyu <bingyu.zhou@siemens.com>
2019-02-14 17:50:46 +0100

    mpc

--- Chance_MPC/objective.cc
+++ Chance_MPC/objective.cc
@@ -1,17 +1,33 @@
 #include "objective.h"
 
+#include <cmath>
+
 using namespace std;
 using namespace Eigen;
 
 namespace cmpc {
 obj::obj() {}
 
-float obj::step_cost(const Eigen::VectorXf& coeff, const float& t_sample) {}
+float obj::step_cost(const Eigen::VectorXf& coeff, const float& t_sample,
+                     const boost::math::cubic_b_spline<float>* ref_path_x,
+                     const boost::math::cubic_b_spline<float>* ref_path_y,
+                     const float& length) {
+  VectorXf terms(6);
+  error(ref_path_x, ref_path_y, terms(0), terms(1));
+
+  terms(2) = m_state[4] * t_sample;
+  terms(3) = m_action[0];
+  terms(4) = m_action[1];
+
+  terms(5) = m_state[4] / length * tan(m_state[3]);
+
+  return coeff.dot(terms);
+}
 
 void obj::error(const boost::math::cubic_b_spline<float>* ref_path_x,
                 const boost::math::cubic_b_spline<float>* ref_path_y,
                 float& e_contour, float& e_lag) {
-  float s = m_state(5);
+  float s = m_state[5];
   float x_ref = (*ref_path_x)(s);
   float y_ref = (*ref_path_y)(s);
 
@@ -23,8 +39,8 @@ void obj::error(const boost::math::cubic_b_spline<float>* ref_path_x,
   float sin_longit = dy_ref / tangent;
 
   e_contour =
-      sin_longit * (m_state(0) - x_ref) - cos_longit * (m_state(1) - y_ref);
-  e_lag = cos_longit * (m_state(0) - x_ref) + sin_longit * (m_state(1) - x_ref);
+      sin_longit * (m_state[0] - x_ref) - cos_longit * (m_state[1] - y_ref);
+  e_lag = cos_longit * (m_state[0] - x_ref) + sin_longit * (m_state[1] - x_ref);
 }
 
 }  // namespace cmpc
--- Chance_MPC/objective.h
+++ Chance_MPC/objective.h
@@ -1,16 +1,21 @@
-#include <boost/math/interpolators/cubic_b_spline.hpp>
 #include "Eigen/Core"
+
+#include <array>
+#include <boost/math/interpolators/cubic_b_spline.hpp>
 
 namespace cmpc {
 class obj {
-  Eigen::VectorXf m_state;   ///< [x, y, \theta, \delta, v, s]
-  Eigen::VectorXf m_action;  ///< [steer_v, acceleration]
+  float m_state[6];   ///< [x, y, \theta, \delta, v, s]
+  float m_action[2];  ///< [steer_v, acceleration]
 
  public:
   obj();
 
   /// step cost
-  float step_cost(const Eigen::VectorXf& coeff, const float& t_sample);
+  float step_cost(const Eigen::VectorXf& coeff, const float& t_sample,
+                  const boost::math::cubic_b_spline<float>* ref_path_x,
+                  const boost::math::cubic_b_spline<float>* ref_path_y,
+                  const float& length);
 
   /// Contour & lag error
   void error(const boost::math::cubic_b_spline<float>* ref_path_x,
